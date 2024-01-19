a69b9e8c54bc5ab94bd1ccd9a294ad276237e378
GarvJaiswal <garvjaiswal08@gmail.com>
2024-01-19 15:49:22 +0530

    [Garv] Add:Added uc3

--- trainingDay7/snake&ladder/snake&ladder/Program.cs
+++ trainingDay7/snake&ladder/snake&ladder/Program.cs
@@ -13,7 +13,22 @@ namespace snake_ladder
             int player_pos = 0;
             Random rand = new Random();
             int dice_num=rand.Next(1,6);
-
+            int option =rand.Next(0,2);
+            switch(option)
+            {
+                case 0:continue;
+                
+                case 1:while(dice_num>0)
+                        {
+                            player_pos++;
+                        }
+                        break;
+                case 2:while(dice_num>0) 
+                        {
+                            player_pos--;
+                        }
+                        break;
+             }
         }
     }
 }
