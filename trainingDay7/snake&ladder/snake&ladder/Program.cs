bb452cc9f97d3d11776f024dad20d397b44cfca4
GarvJaiswal <garvjaiswal08@gmail.com>
2024-01-20 13:15:20 +0530

    [Garv] Add: Added uc7

--- trainingDay7/snake&ladder/snake&ladder/Program.cs
+++ trainingDay7/snake&ladder/snake&ladder/Program.cs
@@ -9,47 +9,107 @@ namespace snake_ladder
     internal class Program
     {
         static void Main(string[] args)
-        {
-            int player_pos = 0;
-            int count = 0;
+        {  
+            int player_pos1 = 0;
+            int player_pos2 = 0;
+            //int count = 0;
+            int i = 1;
             Random rand = new Random();
-            while (player_pos < 100)
+            while (player_pos1 <= 100 || player_pos2<=100)
             {
                 
-                int dice_num = rand.Next(1, 6);
-                count++;
-                int option = rand.Next(1, 3);
+                int dice_num = rand.Next(1, 7);
+                //count++;
+                int option = rand.Next(1, 4);
                 switch (option)
                 {
-                    case 1: 
-                        Console.WriteLine("Position is " + player_pos);
-                        break;
+                    case 1: //no play
+                        if (i == 1)
+                        {
+                            Console.WriteLine("No play:\nPosition of player1 is " + player_pos1);
+                            i = 2;
+                            break;
+                        }
+                        else
+                        { 
+                            Console.WriteLine("No play:\nPosition of player2 is " + player_pos2);
+                            i = 1;
+                            break;
+                        }
 
                     case 2:
-                        while (dice_num > 0)
+                        if (i == 1)
                         {
-                            player_pos++;
-                            dice_num--;
+                            //int newPos1 = player_pos1 + dice_num;
+                            player_pos1 += dice_num;
+                            Console.WriteLine("After ladder:\nPosition of player1 is " + player_pos1);
+                            i = 1;
+                            if (player_pos1 > 100)
+                            {
+                                
+                                player_pos1=player_pos1;
+                            }
                         }
-                        
-                        Console.WriteLine("Position is " + player_pos);
+                        else
+                        {
+                            //int newPos2 = player_pos2 + dice_num;
+                            player_pos2 += dice_num;
+                            Console.WriteLine("After ladder:\nPosition of player2 is " + player_pos2);
+                            i = 2;
+                            if (player_pos2 > 100)
+                            {
+                                player_pos2=player_pos2;
+                            }
+
+                        }
+                       /* if (newPos > 100)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            player_pos = newPos;
+                        }*/
                         break;
-                    case 3:
-                        while (dice_num > 0)
+                    case 3:if (i == 1)
                         {
-                            player_pos--;
-                            dice_num--;
+                            player_pos1 -= dice_num;
+                            Console.WriteLine("After snake:\nPosition is of player1 is " + player_pos1);
+                            i = 2;
+                            if (player_pos1 < 0)
+                            {
+                                player_pos1 = 0;
+                            }
                         }
-                        
-                        Console.WriteLine("Position is " + player_pos);
+                        else
+                        { 
+                            player_pos2 -= dice_num;
+                            Console.WriteLine("After snake:\nPosition is of player2 is " + player_pos2);
+                            i = 1;
+                            if (player_pos2 < 0)
+                            {
+                                    player_pos2 = 0;
+                            }
+                        }               
                         break;
                 }
-                if (player_pos < 0)
-                    player_pos = 0;
-                if (player_pos > 100)
-                    continue;
+                if (player_pos1 == 100)
+                {
+                    Console.WriteLine("Player1 won");
+                    break;
+                }
+                if (player_pos2 == 100)
+                {
+                    Console.WriteLine("Player2 won");
+                    break;
+                }
+
             }
-            Console.WriteLine("Dice was rolled " + count + " times");
+
+
+            //Console.WriteLine("Player Position : " + player_pos);
+            //Console.WriteLine("Dice was rolled " + count + " times");
+
             Console.ReadLine();
         }
     }
